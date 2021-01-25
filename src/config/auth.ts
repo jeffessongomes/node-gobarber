6e8899895014feb06ca9502fb0455e65d4ee538d
Jeffesson Gomes <jeffessongomes@rits.com.br>
2021-01-24 21:15:07 -0300

    Criando middleware de autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9b596e4607a474d72e975b45925afd20',
+    expiresIn: '1d',
+  }
+}
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,7 +1,8 @@
 import {getRepository} from 'typeorm';
 import { compare } from 'bcryptjs';
-
 import { sign } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -31,9 +32,11 @@ class AuthenticateUserService{
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '9b596e4607a474d72e975b45925afd20', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
